2519f7b2ed8e7c2f3e1feb384f628066a321e693
fonfano <fonfano@gmail.com>
2020-10-20 13:15:36 +0200

    Update HFR_Number_the_posts.user.js

--- HFR_Number_the_posts.user.js
+++ HFR_Number_the_posts.user.js
@@ -3,13 +3,14 @@
 // @namespace   github.com/fonfano
 // @match       https://forum.hardware.fr/*
 // @grant       none
-// @version     0.2.1
+// @version     0.2.2
 // @author      Lt Ripley
 // @description Numérote les posts des pages des topics HFR
 // ==/UserScript==
 
 
 //  Historique
+// 20/10/2020   Correction  v 0.2.2  Prise en charge des urls .htm
 // 15/10/2020   Correction  v 0.2.1  Correction d'un bug : page qui plante quand le topic n'a qu'une seule page ('page' n'est pas présent dans l'URL).
 // 07/10/2020   Upgrade.    v 0.2.0  Ajout du nombre total de post depuis le début du topic et changement de numérotation,
 //                                   les posts en haut repris de la page d'avant ont le numéro #0
@@ -17,7 +18,7 @@
 
 
 
-let isUnder = false; // Option de position du numéro de post dans la toolbar : mettre false = à la suite à droite, mettre true = en dessous
+let isUnder = false; // Option de position des numéros de post dans la toolbar : mettre false = à la suite à droite, mettre true = en dessous
 let totalToo = true; // Option pour afficher à coté le total des posts depuis le début.  Mettre true = oui, false = non,
 
 
@@ -28,15 +29,14 @@ if ( document.getElementById("md_arbo_tree_1") && document.getElementById("md_ar
     let url = document.location.href;
     var currentPageNumber = getCurrentPage(url);
     var totalPostsInFullPages = countTotalPostsInFullPages(currentPageNumber);
-
 }
-
 
 let count = 0;
 
-if (currentPageNumber == 1 || currentPageNumber == 0) {
+if (currentPageNumber < 2) {
         count++;
 }
+
 
 for (let toolBar of toolbarCollection) {
 
@@ -46,37 +46,43 @@ for (let toolBar of toolbarCollection) {
     }
     let textToDisplay = document.createTextNode(text);
 
-    if (isUnder) {
-        toolBar.append(textToDisplay);
-    }
+    isUnder ? toolBar.append(textToDisplay) : toolBar.prepend(textToDisplay);
 
-    else {
-        toolBar.prepend(textToDisplay);
-    }
     count++;
 }
 
 
-
-function getCurrentPage(url) {
+function getCurrentPage(url) {  // 2 ifs pour 2 types d'url selon qu'on vienne de la page des drapals ou non
 
     let pageNumberString = "";
 
-    if (url.includes('page') ) {
+    if (url.includes('page=') ) {
 
-            let index = url.indexOf('page')+5;
+            let index = url.indexOf('page=')+5;
 
             while (url.charAt(index) != '&') {
                 pageNumberString += url.charAt(index).toString();
                 index++;
             }
     }
+
     else { pageNumberString = "0";}
 
+    if (url.includes('.htm') ) {
+
+        pageNumberString = "";
+
+        let index2 = url.lastIndexOf('_')+1;
+
+        while (url.charAt(index2) != '.') {
+            pageNumberString += url.charAt(index2).toString();
+            index2++;
+        }
+    }
+
   return Number(pageNumberString);
-
-
 }
+
 
 function countTotalPostsInFullPages(numberOfPages) {
 
