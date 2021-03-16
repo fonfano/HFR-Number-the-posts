73c2ad2a40fc8566bea7b2c14f0166b921a14db7
fonfano <fonfano@gmail.com>
2021-03-16 19:14:00 +0100

    Update HFR_Number_the_posts.user.js

--- HFR_Number_the_posts.user.js
+++ HFR_Number_the_posts.user.js
@@ -3,13 +3,14 @@
 // @namespace   github.com/fonfano
 // @match       https://forum.hardware.fr/*
 // @grant       none
-// @version     0.2.2
+// @version     0.3.0
 // @author      Lt Ripley
 // @description Numérote les posts des pages des topics HFR
 // ==/UserScript==
 
 
 //  Historique
+// 16/03/2020   Upgrade     v 0.3.0  Ajout du numéro de page
 // 20/10/2020   Correction  v 0.2.2  Prise en charge des urls .htm
 // 15/10/2020   Correction  v 0.2.1  Correction d'un bug : page qui plante quand le topic n'a qu'une seule page ('page' n'est pas présent dans l'URL).
 // 07/10/2020   Upgrade.    v 0.2.0  Ajout du nombre total de post depuis le début du topic et changement de numérotation,
@@ -20,6 +21,7 @@
 
 let isUnder = false; // Option de position des numéros de post dans la toolbar : mettre false = à la suite à droite, mettre true = en dessous
 let totalToo = true; // Option pour afficher à coté le total des posts depuis le début.  Mettre true = oui, false = non,
+let pageNumber = true;  // Option pour afficher le numéro de page.  True = activé, False = désactivé
 
 
 let toolbarCollection = document.getElementsByClassName("toolbar");
@@ -31,7 +33,7 @@ if ( document.getElementById("md_arbo_tree_1") && document.getElementById("md_ar
     var totalPostsInFullPages = countTotalPostsInFullPages(currentPageNumber);
 }
 
-let count = 0;
+let count = 0;  // post
 
 if (currentPageNumber < 2) {
         count++;
@@ -40,9 +42,12 @@ if (currentPageNumber < 2) {
 
 for (let toolBar of toolbarCollection) {
 
-    let text = '#'+count;
+    let text = 'msg '+count;
     if (totalToo) {
-        text+=' | Total : '+(totalPostsInFullPages+count);
+        text+=' | total : '+ (totalPostsInFullPages+count);
+    }
+    if (pageNumber)  {
+      text+= ' | page ' + (currentPageNumber);
     }
     let textToDisplay = document.createTextNode(text);
 
@@ -52,8 +57,8 @@ for (let toolBar of toolbarCollection) {
 }
 
 
-function getCurrentPage(url) {  // 2 ifs pour 2 types d'url selon qu'on vienne de la page des drapals ou non
-
+function getCurrentPage(url) {  // recup le numero de page
+                                // 2 ifs pour 2 types d'url selon qu'on vienne de la page des drapals ou non
     let pageNumberString = "";
 
     if (url.includes('page=') ) {
