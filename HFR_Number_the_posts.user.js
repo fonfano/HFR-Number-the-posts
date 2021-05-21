58e2f8fb47cfe741dc08256aaad9f997d50d77f8
fonfano <fonfano@gmail.com>
2021-05-21 13:11:16 +0200

    Update HFR_Number_the_posts.user.js

--- HFR_Number_the_posts.user.js
+++ HFR_Number_the_posts.user.js
@@ -3,112 +3,121 @@
 // @namespace   github.com/fonfano
 // @match       https://forum.hardware.fr/*
 // @grant       none
-// @version     0.4.0
+// @version     0.4.1
 // @author      Lt Ripley
 // @description Numérote les posts des pages des topics HFR et affiche le titre du topic dans les posts
 // ==/UserScript==
 
-// Merci à Roger21 sur HFR pour toute son aide précieuse !
 
+// Merci à Roger21 pour toute son aide précieuse sur HFR !
 
 //  Historique
+// 18/05/2021   Upgrade     v 0.4.1  Ajout de la possibilité de colorer le texte + simplification du code
 // 18/05/2021   Upgrade     v 0.4.0  Ajout du titre du topic 
 // 16/03/2020   Upgrade     v 0.3.0  Ajout du numéro de page
 // 20/10/2020   Correction  v 0.2.2  Prise en charge des urls .htm
 // 15/10/2020   Correction  v 0.2.1  Correction d'un bug : page qui plante quand le topic n'a qu'une seule page ('page' n'est pas présent dans l'URL).
 // 07/10/2020   Upgrade.    v 0.2.0  Ajout du nombre total de post depuis le début du topic et changement de numérotation,
-//                                   les posts en haut repris de la page d'avant ont le numéro #0
+//                                   les posts en haut repris de la page d'avant ont le numéro 0
 // 05/10/2020   Création.   v 0.1.0
 
 
-
-let isUnder = false; // Option de position des numéros de post dans la toolbar : mettre false = à la suite à droite, mettre true = en dessous
+// OPTIONS
+let isUnder = false; // Option de position du texte dans la toolbar : mettre false = à la suite à droite, mettre true = en dessous
 let displayTotal = true; // Option pour afficher à coté le total des posts depuis le début.  Mettre true = oui, false = non,
 let displayPageNumber = true;  // Option pour afficher le numéro de page.  true = activé, False = désactivé
-let displaySujet = true; // option pour afficher le titre du topic.  true = activé, False = désactivé
+let displaySujet = true; // Option pour afficher le titre du topic.  true = activé, False = désactivé
+let numbersColour = '#000000' // Option pour colorer les chiffres, exemples : '#000000' = noir d'origine, et '#F18F18' = orange HFR
+let titleColour = '#000000'  // Option pour colorer le titre, exemples : '#000000' = noir d'origine, et '#F18F18' = orange HFR
+//
 
 
 let toolBarCollection = document.getElementsByClassName("toolbar");
 
 if ( document.getElementById("md_arbo_tree_1") && document.getElementById("md_arbo_tree_b_1") ) {
 
-    let url = document.location.href;
-    var currentPageNumber = getCurrentPage(url);
-    var totalPostsInFullPages = countTotalPostsInFullPages(currentPageNumber);
-    let sujet ='';
-    if (displaySujet) {getSujet();}
+  let url = document.location.href;
+  var currentPageNumber = getCurrentPage(url);
+  var totalPostsInFullPages = countTotalPostsInFullPages(currentPageNumber);
+  if (displaySujet) {var sujet = getSujet();}
 }
 
 let count = 0;  // post
 
-if (currentPageNumber < 2) {
-        count++;
-}
+if (currentPageNumber < 2) { count++; }
 
 
 for (let toolBar of toolBarCollection) {
   
-    let text = 'msg '+count;
+  let text = 'msg '+count;
+
+  if (displayTotal)  {
+    text+= ' | total : '+ (totalPostsInFullPages+count);
+  }
+
+  if (displayPageNumber)  {
+    text+= ' | page ' + (currentPageNumber);
+  }
   
-    if (displayTotal)  {
-      text+=' | total : '+ (totalPostsInFullPages+count);
-    }
-  
-    if (displayPageNumber)  {
-      text+= ' | page ' + (currentPageNumber);
-    }
-  
-    let maDiv = document.createElement("div");
-    if (!isUnder)  {  
-      maDiv.setAttribute("class", "left" );
-    }
-    maDiv.appendChild(document.createTextNode(text));  // nombres
-    maDiv.style.marginLeft = "20px"; 
-  
-    if(displaySujet)  {
-      
-      let monSpan =  document.createElement("span" );
-      monSpan.appendChild(document.createTextNode(sujet)); // le titre du topic
-      monSpan.style.marginLeft = "20px";
-      //monSpan.style.marginRight = "5px";
-      //monSpan.style.color= "pink";
-      maDiv.appendChild(monSpan);
-      
-    }
-  
-    isUnder ? toolBar.appendChild(maDiv) : toolBar.insertBefore(maDiv, toolBar.querySelector("div.spacer" ))  // pour insérer avant le spacer
-    
-    count++;
+  let maDiv = document.createElement("div");
+
+  maDiv.appendChild(document.createTextNode(text));  // nombres
+  maDiv.style.color = numbersColour;
+
+  if(displaySujet)  {
+    let monSpan =  document.createElement("span" );
+    monSpan.appendChild(document.createTextNode(sujet)); // le titre du topic
+    monSpan.style.marginLeft = "20px";
+    monSpan.style.color = titleColour;
+    maDiv.appendChild(monSpan);
+  }
+
+  if (!isUnder)  {  
+    maDiv.setAttribute("class", "left" );
+    maDiv.style.marginLeft = "20px";
+    toolBar.insertBefore(maDiv, toolBar.querySelector("div.spacer" ));  // insérer avant le spacer
+  }
+
+  else {
+    maDiv.style.marginLeft = "0px";
+    toolBar.appendChild(maDiv);
+  }
+
+  count++;
 }
 
+  
+  
+  
+  
 
 function getCurrentPage(url) {  // recup le numero de page
                                 // 2 ifs pour 2 types d'url selon qu'on vienne de la page des drapals ou non
-    let pageNumberString = "";
+  let pageNumberString = "";
 
-    if (url.includes('page=') ) {
+  if (url.includes('page=') ) {
 
-            let index = url.indexOf('page=')+5;
+          let index = url.indexOf('page=')+5;
 
-            while (url.charAt(index) != '&') {
-                pageNumberString += url.charAt(index).toString();
-                index++;
-            }
-    }
+          while (url.charAt(index) != '&') {
+              pageNumberString += url.charAt(index).toString();
+              index++;
+          }
+  }
 
-    else { pageNumberString = "0";}
+  else { pageNumberString = "0";}
 
-    if (url.includes('.htm') ) {
+  if (url.includes('.htm') ) {
 
-        pageNumberString = "";
+      pageNumberString = "";
 
-        let index2 = url.lastIndexOf('_')+1;
+      let index2 = url.lastIndexOf('_')+1;
 
-        while (url.charAt(index2) != '.') {
-            pageNumberString += url.charAt(index2).toString();
-            index2++;
-        }
-    }
+      while (url.charAt(index2) != '.') {
+          pageNumberString += url.charAt(index2).toString();
+          index2++;
+      }
+  }
 
   return Number(pageNumberString);
 }
@@ -116,13 +125,13 @@ function getCurrentPage(url) {  // recup le numero de page
 
 function countTotalPostsInFullPages(numberOfPages) {
 
-    let totalOfPosts=0;
-    if (numberOfPages > 1 ) {
-        totalOfPosts =(numberOfPages-1)*40;
-    }
-    else {totalOfPosts = 0;}
+  let totalOfPosts=0;
+  if (numberOfPages > 1 ) {
+      totalOfPosts =(numberOfPages-1)*40;
+  }
+  else {totalOfPosts = 0;}
 
-    return totalOfPosts;
+  return totalOfPosts;
 }
 
 
@@ -133,6 +142,8 @@ function getSujet ()  {
   let indexForName1 = preSujet.indexOf('<h3>')+4;
   let indexForName2 = preSujet.indexOf('</h3>');
   
-  sujet = preSujet.substring(indexForName1, indexForName2);  
+  let title = preSujet.substring(indexForName1, indexForName2); 
+  
+  return title;
   
 }
